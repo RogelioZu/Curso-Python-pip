fd56836d03d08ae705ab6d520d58fb333851d416
Roger_Zuriaga <rogerdroid1700@gmail.com>
2025-01-16 13:17:12 -0600

    agregamos la funcionalidad a la graficacion por pais

--- app/charts_app.py
+++ app/charts_app.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plot
 
-def generate_bar_chart(labels, values):
+def generate_bar_chart(name, labels, values):
 
     fig, ax  = plot.subplots()
     ax.bar(labels,values)
-    plot.savefig('bar.png')
-    plot.close()
+    plot.savefig(f'app/images/{name}.png')
+    plot.close()    
 
 
-def generate_pie_chart(labels, values):
+def generate_pie_chart(name, labels, values):
 
     fig, ax = plot.subplots()
     ax.pie(values , labels = labels)
     ax.axis('equal')
-    plot.savefig('pie.png')
+    plot.savefig(f'app/images/{name}.png')
     plot.close()
--- app/main.py
+++ app/main.py
@@ -1,21 +1,34 @@
 
 import charts_app
 import csv_reader
+import util
 
 
-
-data = 'app/data.csv'
+file = 'app/data.csv'
 
 
 def run():
 
-    data2 = csv_reader.read(data)
-    data2 = list(filter(lambda item: item["Continent"] == "South America", data2))
-    countries = list(map(lambda x: x['Country/Territory'], data2))
-    percentages = list(map(lambda x: x['World Population Percentage'],data2))
+    data = csv_reader.read(file)
+    data = list(filter(lambda item: item["Continent"] == "South America", data))
+    countries = list(map(lambda x: x['Country/Territory'], data))
+    percentages = list(map(lambda x: x['World Population Percentage'],data))
+
+
+    country = input("Type country => ").title()
+
+    result = util.population_by_country(data, country)
+
+    if len(result) > 0:
+        country = result[0]
+        print(country['Country/Territory'])
+        keys, values = util.get_population(country)
+        charts_app.generate_bar_chart(country['Country/Territory'],keys, values)
+
+
     
-    charts_app.generate_pie_chart(countries,percentages)
-    
+    charts_app.generate_pie_chart('South America',countries,percentages)
+
 if __name__ == '__main__':
     run()
 
